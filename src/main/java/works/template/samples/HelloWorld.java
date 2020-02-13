4810909b0f645bcb4135bc57fc53551205ac4a9b
Amogh Jagadale <a.jagadale@wirecard.com>
2020-02-13 16:09:27 +0100

    [version:1.2] Printing hello world

--- src/main/java/works/template/samples/HelloWorld.java
+++ src/main/java/works/template/samples/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/plain");
         PrintWriter writer = response.getWriter();
-        writer.print("Hello Amogh Jagadale!");
+        writer.print("Hello World!");
         writer.close();
     }
 }
